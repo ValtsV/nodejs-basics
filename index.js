43ceaa78e4dca66d4d928ac9b3ed692fbe6b73e9
Valts Vilcans <valts03@inbox.lv>
2020-06-29 18:42:37 +0200

    [node server w/ json]

--- index.js
+++ index.js
@@ -19,16 +19,22 @@ const path = require("path");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.readFile(
-      path.join(__dirname, "public", "index.html"),
-      (err, content) => {
-        if (err) throw err;
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
-      }
-    );
-  }
+  //   if (req.url === "/api/users") {
+  // fs.readFile(
+  //   path.join(__dirname, "public", "index.html"),
+  //   (err, content) => {
+  //     if (err) throw err;
+  //     res.writeHead(200, { "Content-Type": "text/html" });
+  //     res.end(content);
+  //   }
+  // );
+  //     const users = [
+  //       { name: "Bob Smith", age: 35 },
+  //       { name: "John Doe", age: 32 },
+  //     ];
+  //     res.writeHead(200, { "Content-Type": "application/json" });
+  //     res.end(JSON.stringify(users));
+  //   }
 });
 
 const PORT = process.env.PORT || 5000;
