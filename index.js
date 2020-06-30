cdabe805e1b35ed1da1c05e582059adb50beb06b
Valts Vilcans <valts03@inbox.lv>
2020-06-30 12:41:31 +0200

    [basic server 2]

--- index.js
+++ index.js
@@ -17,6 +17,7 @@
 const http = require("http");
 const path = require("path");
 const fs = require("fs");
+const { dirname } = require("path");
 
 const server = http.createServer((req, res) => {
   //   if (req.url === "/api/users") {
@@ -35,6 +36,67 @@ const server = http.createServer((req, res) => {
   //     res.writeHead(200, { "Content-Type": "application/json" });
   //     res.end(JSON.stringify(users));
   //   }
+
+  console.log(req.url);
+
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  //  Extension of the file
+
+  let extname = path.extname(filePath);
+
+  // initial content type
+
+  let contentType = "text/html";
+
+  // check the extension
+
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+
+  // read file
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == "ENOENT") {
+        // Page not found
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(content, "utf8");
+          }
+        );
+      } else {
+        // Some server error
+        res.writeHead(500);
+        res.end(`Server error: ${err.code}`);
+      }
+    } else {
+      // success
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content, "utf8");
+    }
+  });
 });
 
 const PORT = process.env.PORT || 5000;
